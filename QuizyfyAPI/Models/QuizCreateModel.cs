03dde6f31aa7b6708103c3f9b540c2345476dede
Adrian Gaborek <unity3d09@gmail.com>
2019-06-27 18:37:37 +0200

    Added full documentation for quizzes controller. Created model for creating and updating quizes and mapping for it.

---
+++ QuizyfyAPI/Models/QuizCreateModel.cs
@@ -0,0 +1,26 @@
+﻿using QuizyfyAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizyfyAPI.Models
+{
+    /// <summary>
+    /// A quiz with name and questions properties.
+    /// </summary>
+    public class QuizCreateModel
+    {
+        /// <summary>
+        /// Quiz name.
+        /// </summary>
+        [Required]
+        [MaxLength(70)]
+        public string Name { get; set; }
+        /// <summary>
+        /// Collection of questions which belongs to quiz.
+        /// </summary>
+        public ICollection<QuestionModel> Questions { get; set; }
+    }
+}
