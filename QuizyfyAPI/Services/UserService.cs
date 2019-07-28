1901983286f08fc0b346688d7d156b61f7633583
Adrian Gaborek <unity3d09@gmail.com>
2019-07-28 23:38:44 +0200

    Added refresh token functionality. Created refresh endpoint on user controller. Created entity to store refresh token in db. Created repository for refresh tokens. Created mapping profile for refresh tokens and user with refresh tokens. Started refactoring user controller by moving code to service. Got rid of JWTHelper and moved code to service. Added migrations for refresh tokens. Created service for user controller. Registered token validation parameters for di framework.

---
+++ QuizyfyAPI/Services/UserService.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using QuizyfyAPI.Data;
+using QuizyfyAPI.Domain;
+using QuizyfyAPI.Helpers;
+using QuizyfyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizyfyAPI.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IRefreshTokenRepository _refreshTokenRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly AppSettings _appSettings;
+        private readonly TokenValidationParameters _tokenValidationParameters;
+
+        public UserService(IRefreshTokenRepository refreshTokenRepository, IUserRepository userRepository, IOptions<AppSettings> appSettings, TokenValidationParameters tokenValidationParameters)
+        {
+            _refreshTokenRepository = refreshTokenRepository;
+            _userRepository = userRepository;
+            _appSettings = appSettings.Value;
+            _tokenValidationParameters = tokenValidationParameters;
+        }
+
+        public async Task<AuthenticationResult> RefreshTokenAsync(UserRefreshModel model)
+        {
+            var validatedToken = GetPrincipalFromToken(model.JwtToken);
+
+            if (validatedToken == null)
+            {
+                return new AuthenticationResult { Errors = new[] { "Invalid token" } };
+            }
+
+            var expiryDateUnix = long.Parse(validatedToken.Claims.Single(x => x.Type == JwtRegisteredClaimNames.Exp).Value);
+            var expiryDateUtc = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(expiryDateUnix);
+
+            if (expiryDateUtc > DateTime.UtcNow)
+            {
+                return new AuthenticationResult { Errors = new[] { "This token hasn't expired yet" } };
+            }
+
+            var jti = validatedToken.Claims.Single(x => x.Type == JwtRegisteredClaimNames.Jti).Value;
+
+            var storedRefreshToken = await _refreshTokenRepository.GetRefreshToken(model.RefreshToken.Token);
+
+            if (storedRefreshToken == null)
+            {
+                return new AuthenticationResult { Errors = new[] { "This refresh token doesn't exist" } };
+            }
+
+            if (DateTime.UtcNow > storedRefreshToken.ExpiryDate)
+            {
+                return new AuthenticationResult { Errors = new[] { "This refresh token has expired" } };
+            }
+
+            if (storedRefreshToken.Invalidated)
+            {
+                return new AuthenticationResult { Errors = new[] { "This refresh token has been invalidated" } };
+            }
+
+            if (storedRefreshToken.Used)
+            {
+                return new AuthenticationResult { Errors = new[] { "This refresh token has been used" } };
+            }
+
+            if (storedRefreshToken.JwtId != jti)
+            {
+                return new AuthenticationResult { Errors = new[] { "This refresh token does not match this JWT" } };
+            }
+
+            storedRefreshToken.Used = true;
+            _refreshTokenRepository.UpdateRefreshToken(storedRefreshToken);
+
+            await _userRepository.SaveChangesAsync();
+
+            var user = await _userRepository.GetUserById(Int32.Parse(validatedToken.Claims.Single(x => x.Type == ClaimTypes.Name).Value));
+
+            user = await RequestToken(user);
+
+
+            return new AuthenticationResult
+            {
+                Success = true,
+                User = user
+            };
+        }
+
+        public async Task<User> RequestToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                }),
+
+                Expires = DateTime.UtcNow.AddMinutes(15),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            user.RefreshToken = new RefreshToken()
+            {
+                UserId = user.Id,
+                JwtId = token.Id,
+                CreationDate = DateTime.UtcNow,
+                ExpiryDate = DateTime.UtcNow.AddMonths(1)
+            };
+            user.JwtToken = tokenHandler.WriteToken(token);
+
+            _refreshTokenRepository.Add(user.RefreshToken);
+            await _userRepository.SaveChangesAsync();
+            await _refreshTokenRepository.SaveChangesAsync();
+            return user;
+        }
+
+        private ClaimsPrincipal GetPrincipalFromToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            try
+            {
+                _tokenValidationParameters.ValidateLifetime = false;
+                var principal = tokenHandler.ValidateToken(token, _tokenValidationParameters, out var validatedToken);
+                var boole = IsJwtWithValidSecurityAlgorithm(validatedToken);
+                if (!IsJwtWithValidSecurityAlgorithm(validatedToken))
+                {
+                    return null;
+                }
+
+                return principal;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        private bool IsJwtWithValidSecurityAlgorithm(SecurityToken validatedToken)
+        {
+            return (validatedToken is JwtSecurityToken jwtSecurityToken) &&
+                   jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256,
+                       StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
