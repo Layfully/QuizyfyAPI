3727a82d936248eb1b9d04a548f67d24a54e28de
Adrian Gaborek <unity3d09@gmail.com>
2019-07-30 20:57:16 +0200

    Refactored user controller to be production ready: -Deleted unused usings, -Moved business code to service, -Got rid of unused dependencies, -Updated swagger documentation, -Optimized some things like update.
    Added Update method to IRepository interface.
    Implemented Update method in user repository.
    
    Added domain results:
    -basic result (returns only info if request suceeded and error information)
    -detailed result (basic result + info if entity was found)
    -object result (detailed result + entity)

---
+++ QuizyfyAPI/Domain/ObjectResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizyfyAPI.Domain
+{
+    public class ObjectResult<T> : DetailedResult
+    {
+        public T Object { get; set; }
+    }
+}
