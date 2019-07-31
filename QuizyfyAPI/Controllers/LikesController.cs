fd94425d3b68bc6928ef8930012211b34470ee8e
Adrian Gaborek <unity3d09@gmail.com>
2019-07-31 20:07:32 +0200

    Refactored user controller to be production ready: -Deleted unused usings, -Added produces / consumes attributes. -Moved business code to service, -Got rid of unused dependencies, -Updated swagger documentation,

--- QuizyfyAPI/Controllers/LikesController.cs
+++ QuizyfyAPI/Controllers/LikesController.cs
@@ -1,134 +1,107 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
-using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using QuizyfyAPI.Data;
 using QuizyfyAPI.Models;
+using QuizyfyAPI.Services;
 
 namespace QuizyfyAPI.Controllers
 {
-    [Route("api/Quizzes/{quizId}/[controller]")]
     [Authorize]
+    [Route("api/Quizzes/{quizId}/[controller]")]
+    [Produces("application/json")]
+    [Consumes("application/json")]
     [ApiController]
     public class LikesController : ControllerBase
     {
-        private readonly IQuizRepository _quizRepository;
-        private readonly ILikeRepository _likeRepository;
-        private readonly IMapper _mapper;
+        private readonly ILikeService _likeService;
+        private int UserId => int.Parse(User.Claims.Single(x => x.Type == "Id").Value);
 
-        public LikesController(IQuizRepository quizRepository, ILikeRepository likeRepository, IMapper mapper)
+        public LikesController(ILikeService likeService)
         {
-            _quizRepository = quizRepository;
-            _likeRepository = likeRepository;
-            _mapper = mapper;
+            _likeService = likeService;
         }
 
         /// <summary>
-        /// Updates question for given quiz with specified id and data.
+        /// Likes quiz.
         /// </summary>
-        /// <param name="quizId">Id of quiz you want to update.</param>
-        /// <param name="questionId">Id of question you want to update.</param>
-        /// <param name="model">New data for question.</param>
-        /// <returns>>An ActionResult of QuestionModel</returns>
+        /// <param name="quizId">Id of quiz you want to like.</param>
+        /// <returns>Action Result with like model</returns>
         /// <remarks>
-        /// Sample request (this request returns **updated question**)  
+        /// Sample request (this request returns **like**)  
         ///      
-        ///     PUT /quizzes/1/questions/1
+        ///     PUT /quizzes/1/likes
         ///     
-        ///     {
-        ///         "name":"another name"
-        ///     }
-        ///     
-        /// </remarks>  
-        /// <response code="200">Returns question with provided id and updated info.</response>
-        /// <response code="404">Question with provided id wasn't found.</response> 
-        /// <response code="204">Probably should never return that but there is possibility that question isn't null but mapping result in null.</response> 
-        /// <response code="422">One of validation errors occured.</response>
-        /// <response code="400">Bad request not complete or corrupted data.</response>
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        /// </remarks> 
+        /// <response code="200">Returns like which existed or newly created.</response>
+        /// <response code="400">There was no records updated.</response>
+        /// <response code="401">You aren't authenticated. Please authenticate first.</response>
+        /// <response code="404">Quiz with given id was not found.</response>
+        /// <response code="406">Request data type is not in acceptable format.</response>
+        /// <response code="422">Request data couldn't be processed.</response>
+        /// <response code="500">Something threw exception on server.</response>
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status406NotAcceptable)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpPut]
         public async Task<ActionResult<LikeModel>> Put(int quizId)
         {
-            var userId = Int32.Parse(User.Identity.Name);
+            var likeResponse = await _likeService.Like(quizId, UserId);
 
-            var quiz = await _quizRepository.GetQuiz(quizId);
-
-            if (quiz == null)
+            if (!likeResponse.Success)
             {
-                return NotFound();
+                if (!likeResponse.Found)
+                {
+                    return NotFound(likeResponse.Errors);
+                }
+                return BadRequest(likeResponse.Errors);
             }
-
-            var like = await _likeRepository.GetLike(quizId, userId);
-
-            if (like != null)
-            {
-                return Ok();
-            }
-
-            var model = new LikeModel
-            {
-                QuizId = quizId,
-                UserId = userId
-            };
-
-            _mapper.Map(model, like);
-
-            _likeRepository.Add(like);
-
-            if (await _likeRepository.SaveChangesAsync())
-            {
-                return _mapper.Map<LikeModel>(like);
-            }
-
-            return BadRequest();
+            return likeResponse.Object;
         }
 
         /// <summary>
-        /// Deletes quiz with specified id.
+        /// Unlikes quiz.
         /// </summary>
-        /// <param name="id">Id of quiz you want to delete.</param>
+        /// <param name="quizId">Id of quiz you want to unlike.</param>
         /// <returns>Response Code</returns>
         /// <remarks>
         /// Sample request (this request returns **response code only**)  
         ///      
-        ///     DELETE /quizzes/1
+        ///     DELETE /quizzes/1/likes
         ///     
         /// </remarks> 
-        /// <response code = "404" >Quiz with provided id wasn't found.</response> 
-        /// <response code = "200" >Quiz was sucessfully deleted.</response> 
-        /// <response code = "400" >Request data was not complete or corrupted.</response> 
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        /// <response code="200">Returns ok if like was deleted.</response>
+        /// <response code="400">There was no affected records in db.</response>
+        /// <response code="401">You aren't authenticated. Please authenticate first.</response>
+        /// <response code="404">Quiz with given id was not found.</response>
+        /// <response code="422">Request data couldn't be processed.</response>
+        /// <response code="500">Something threw exception on server.</response>
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status406NotAcceptable)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int quizId)
         {
-            var quiz = await _quizRepository.GetQuiz(quizId);
-            var userId = Int32.Parse(User.Identity.Name);
+            var deleteResponse = await _likeService.Delete(quizId, UserId);
 
-            if (quiz == null)
+            if (!deleteResponse.Success)
             {
-                return NotFound();
+                if (!deleteResponse.Found)
+                {
+                    return NotFound(deleteResponse.Errors);
+                }
+                return BadRequest(deleteResponse.Errors);
             }
-
-            var like = await _likeRepository.GetLike(quizId, userId);
-
-            _likeRepository.Delete(like);
-
-            if (await _likeRepository.SaveChangesAsync())
-            {
-                return Ok();
-            }
-
-            return BadRequest();
+            return Ok();
         }
     }
 }
