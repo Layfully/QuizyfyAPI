58b67f06c904a5ce254226ad8a9f344a4f0083b9
Adrian Gaborek <unity3d09@gmail.com>
2019-10-02 22:07:01 +0200

    docs: fill missing docs for request and response contracts.

--- QuizyfyAPI/Contracts/Responses/LikeResponse.cs
+++ QuizyfyAPI/Contracts/Responses/LikeResponse.cs
@@ -1,8 +1,17 @@
 ﻿namespace QuizyfyAPI.Contracts.Responses
 {
+    /// <summary>
+    /// A like of quiz which belongs to some user.
+    /// </summary>
     public class LikeResponse
     {
+        /// <summary>
+        /// This id specifies to which quiz like belongs to.
+        /// </summary>
         public int QuizId { get; set; }
+        /// <summary>
+        /// This id specifies to which user liked quiz.
+        /// </summary>
         public int UserId { get; set; }
     }
 }
