ca4c34d1459b991d0e34d65b5010f9b32ff4b943
Adrian Gaborek <adrian.gaborek@student.up.krakow.pl>
2022-03-26 15:34:11 +0100

    fix: remove file in wrong location

---
+++ QuizyfyAPI/Data/IChoiceRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace QuizyfyAPI.Data;
+public interface IChoiceRepository : IRepository
+{
+    Task<Choice[]> GetChoices(int questionId);
+    Task<Choice> GetChoice(int questionId, int choiceId);
+}
+
