5cf71a306a189e96bbd0286e4ef0ac525baee102
Adrian Gaborek <adriangaborek3@gmail.com>
2022-03-27 13:13:21 +0200

    Ci/cd (#34)
    * fix: remove secrets from repo

--- QuizyfyAPI/Contracts/Requests/QuestionCreateRequest.cs
+++ QuizyfyAPI/Contracts/Requests/QuestionCreateRequest.cs
@@ -1,19 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using QuizyfyAPI.Contracts.Responses;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-namespace QuizyfyAPI.Contracts.Requests;
-/// <summary>
-/// Question with text(actual question) only. Used for DTO.
-/// </summary>
-public class QuestionCreateRequest
+namespace QuizyfyAPI.Contracts.Requests
 {
     /// <summary>
-    /// Question text.
+    /// Question with text(actual question) only. Used for DTO.
     /// </summary>
-    [Required]
-    [MaxLength(70)]
-    public string Text { get; set; }
+    public class QuestionCreateRequest
+    {
+        /// <summary>
+        /// Question text.
+        /// </summary>
+        [Required]
+        [MaxLength(70)]
+        public string Text { get; set; }
 
-    public int ImageId { get; set; } 
+        public int ImageId { get; set; } 
 
-    public virtual ICollection<ChoiceCreateRequest> Choices { get; set; }
+        public virtual ICollection<ChoiceCreateRequest> Choices { get; set; }
+    }
 }
