cf09c10082d7929e926338a5d132021051a41a59
Adrian Gaborek <adriangaborek3@gmail.com>
2022-04-06 15:27:26 +0200

    fix: add migrations after nullable reference types were introduced (#39)

--- QuizyfyAPI/Migrations/20220406132043_Nullable reference types.Designer.cs
+++ QuizyfyAPI/Migrations/20220406132043_Nullable reference types.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuizyfyAPI.Data;
 
@@ -11,9 +12,10 @@ using QuizyfyAPI.Data;
 namespace QuizyfyAPI.Migrations
 {
     [DbContext(typeof(QuizDbContext))]
-    partial class QuizDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220406132043_Nullable reference types")]
+    partial class Nullablereferencetypes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
